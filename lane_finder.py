a5640e1a4566f1cd8167215f382de282586189dd
Jie Hou <jie@pony.ai>
2017-09-02 02:06:37 -0700

    WIP: now can generate video

---
+++ lane_finder.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('camera_cal/*.jpg')
+
+# Step through the list and search for chessboard corners
+for index, filename in enumerate(images):
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+
+    # If found, add object points, image points
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        # cv2.drawChessboardCorners(img, (9,6), corners, ret)
